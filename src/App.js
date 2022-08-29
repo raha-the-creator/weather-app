6d2a4a69d400e417f31464c2f872dc47269cc3f2
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-08-29 13:12:59 -0700

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([])
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Burnaby&appid=b2d920699bdff95168ac769bec7938de`
+  
+  const getData = async () => {
+    const { data } = await axios.get(url)
+    setData(data)
+  }
+
+  useEffect(() => {
+    getData()
+    console.log(data);
+  }, [])
+
+  // axios.get(url).then((response) => {
+  //   setData(response.data)
+  //   console.log(response.data)
+  // })
+
+
   return (
-    <div className="">
-      Weather App
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p></p>
+            <img 
+              alt='weather'
+              src={`icons/${data.weather[0].icon}.png`}
+            />
+            <h1>Temp: {data.main.temp}°F</h1>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <p>Feels</p>
+        </div>
+      </div>
     </div>
   );
 }
